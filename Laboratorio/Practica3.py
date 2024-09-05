8ad9a80914ab8519733c5b472f8e78e5235a12df
IanPescador <ian.pescador@uabc.edu.mx>
2024-09-04 19:50:54 -0700

    TCP_SERVER ADDED

---
+++ Laboratorio/Practica3.py
@@ -0,0 +1,30 @@
+import socket
+
+HOST = '192.168.4.88'
+PORT = 3333
+
+def main():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        # Conectar al servidor
+        sock.connect((HOST, PORT))
+        print(f"Conectado a {HOST}:{PORT}")
+
+        while True:
+            MESSAGE = input("Por favor, ingresa el mensaje que deseas enviar: ")
+            
+            # Enviar mensaje
+            print(f"Enviando mensaje: {MESSAGE}")
+            sock.sendall(MESSAGE.encode())
+
+            # Esperar y recibir respuesta
+            data = sock.recv(1024)
+            print(f"Mensaje recibido: {data.decode()}")
+            
+    except socket.error as e:
+        print(f"Error de socket: {e}")
+    finally:
+        sock.close()
+
+if __name__ == "__main__":
+    main()
